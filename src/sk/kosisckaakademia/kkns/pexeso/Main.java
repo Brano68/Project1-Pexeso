d1415949441557dcd81fd5d5dafa8e90d4fb925f
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2020-11-28 21:07:33 +0100

    added a next comment

--- src/sk/kosisckaakademia/kkns/pexeso/Main.java
+++ src/sk/kosisckaakademia/kkns/pexeso/Main.java
@@ -8,7 +8,7 @@ it is a class Main for a main method, for creating objects and static methods
 
 public class Main {
     public static void main(String[] args) {
-    //pridavam koment
+    //pridavam koment na dalsi test
 
     }
 }

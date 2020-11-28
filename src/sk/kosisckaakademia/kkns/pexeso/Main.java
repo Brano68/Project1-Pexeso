820582ea2ec2cba4608b689daf50c1a233de2889
Simon Senic <simon.senic@kosickaakademia.sk>
2020-11-28 17:32:46 +0100

    Push test

--- src/sk/kosisckaakademia/kkns/pexeso/Main.java
+++ src/sk/kosisckaakademia/kkns/pexeso/Main.java
@@ -4,6 +4,8 @@ package sk.kosisckaakademia.kkns.pexeso;
 it is a class Main for a main method, for creating objects and static methods
  */
 
+//PUSH TEST
+
 public class Main {
     public static void main(String[] args) {
 

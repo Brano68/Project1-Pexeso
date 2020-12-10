3146db4c94145edc906d70404a78955467f1faf6
Simon Senic <simon.senic@kosickaakademia.sk>
2020-12-10 16:26:43 +0100

    Updated method fillFieldWithAlphabet()

--- src/sk/kosisckaakademia/kkns/pexeso/FieldGenerated.java
+++ src/sk/kosisckaakademia/kkns/pexeso/FieldGenerated.java
@@ -38,6 +38,22 @@ public class FieldGenerated extends Field{
         String card = "Aa";
 
 
+        for(int x=0; x<arr.length; x++) {
+            int count=0;
+            for (int i = 0; i < field.length; i++) {
+                for (int j=0; j<field[i].length; j++) {
+                    count++;
+                    if (count == arr[x]) {
+                        field[i][j] = card;
+                        card = String.valueOf((char)(card.charAt(0)+1)) + String.valueOf((char)(card.charAt(1)+1));
+                    }
+                }
+            }
+        }
+
+
+
+
     }
 
     //a method for filling the field with black Peters &&

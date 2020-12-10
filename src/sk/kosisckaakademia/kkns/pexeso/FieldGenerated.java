1b4687c4f4a938a9ea6b531ac97296d225326200
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2020-12-10 11:35:52 +0100

    added one method and package for trying

--- src/sk/kosisckaakademia/kkns/pexeso/FieldGenerated.java
+++ src/sk/kosisckaakademia/kkns/pexeso/FieldGenerated.java
@@ -15,7 +15,21 @@ public class FieldGenerated extends Field{
 
     //a method which is used for filling an array with white spaces
     private void fillFieldWithEmptySpaces(){
+        for(int i = 0; i < field.length; i++){
+            for(int j = 0; j < field[0].length; j++){
+                field[i][j] = " ";
+            }
+        }
+    }
 
+    //ukazka testovania metoda sa zmaze sluzi iba na testing
+    public String[][] fillFieldWithEmptySpacess(){
+        for(int i = 0; i < field.length; i++){
+            for(int j = 0; j < field[0].length; j++){
+                field[i][j] = " ";
+            }
+        }
+        return field;
     }
 
     //a method for generating 52numbers without repeating
@@ -31,6 +45,21 @@ public class FieldGenerated extends Field{
 
         return arr;
     }
+
+    //ukazka testovania metoda sa zmaze sluzi iba na testing
+    public int[] generateArrayRandomNumbersFrom0To56With52Numberss(){
+        int[] arr = new int[52];
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<arr.length; i++){
+            for(;;){
+                int num = new Random().nextInt(57);
+                if(set.add(num)==true){ arr[i]=num; break; }
+            }
+        }
+
+        return arr;
+    }
+
 
     //a method for filling the field with alphabet
     private void fillFieldWithAlphabet(){

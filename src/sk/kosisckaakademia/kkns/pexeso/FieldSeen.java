01ce6fc10d637ed3e0ec61c2dc2802a73dfea3b3
Simon Senic <simon.senic@kosickaakademia.sk>
2020-12-12 00:52:20 +0100

    Updated method fillFieldWithNumbers()

--- src/sk/kosisckaakademia/kkns/pexeso/FieldSeen.java
+++ src/sk/kosisckaakademia/kkns/pexeso/FieldSeen.java
@@ -10,7 +10,10 @@ public class FieldSeen extends Field {
 
     //a method for filling a field with numbers from 0 to 55
     private void fillFieldWithNumbers(){
-
+        int count=0;
+        for(int i=0; i< field.length; i++)
+            for(int j=0; j<field[0].length; j++)
+                field[i][j]=String.valueOf(count++);
     }
 
 
@@ -27,10 +30,14 @@ public class FieldSeen extends Field {
 
     //test
     public <T extends Player> void paintField(Player1 player1, T player2){
+        String[][] f = getField();
+
         System.out.println();
         System.out.println("+-------------------------+" +"  "
                 +"\u001B[34m" +player1.getName() +": " +player1.getScore() +" "
                 +"\u001B[31m" +player2.getName() +": " +player2.getScore() +"\u001B[0m");
+        System.out.print("| ");
+        for(int j=0; j<f[0].length; j++) System.out.print(f[0][j]);
     }
 
 

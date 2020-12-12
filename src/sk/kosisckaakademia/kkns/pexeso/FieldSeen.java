b9eaab9e9aa656c6a2c16eae45c20b9abdbb82d2
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2020-12-12 18:53:50 +0100

    generic method was finished

--- src/sk/kosisckaakademia/kkns/pexeso/FieldSeen.java
+++ src/sk/kosisckaakademia/kkns/pexeso/FieldSeen.java
@@ -2,6 +2,18 @@ package sk.kosisckaakademia.kkns.pexeso;
 
 public class FieldSeen extends Field {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+
+
     //constructor
     public FieldSeen(){
         fillFieldWithNumbers();
@@ -30,6 +42,7 @@ public class FieldSeen extends Field {
 
     //test
     public <T extends Player> void paintField(Player1 player1, T player2){
+/*
         String[][] f = getField();
 
         System.out.println();
@@ -38,7 +51,40 @@ public class FieldSeen extends Field {
                 +"\u001B[31m" +player2.getName() +": " +player2.getScore() +"\u001B[0m");
         System.out.print("| ");
         for(int j=0; j<f[0].length; j++) System.out.print(f[0][j]);
+
+
+
+ */
+
+        String border = "###########################";
+        System.out.println("\t" + ANSI_GREEN_BACKGROUND + border + ANSI_RESET + " " +
+                ANSI_PURPLE + player1.getName() + " Score:" + player1.getScore() + ANSI_RESET + "\t" +
+                ANSI_BLUE + player2.getName() + " Score:" + player2.getScore() + ANSI_RESET);
+        for(int i = 0; i < field.length; i++){
+            System.out.print("\t" + ANSI_GREEN_BACKGROUND + "#" + ANSI_RESET);
+            System.out.print(ANSI_CYAN_BACKGROUND + " " + ANSI_RESET);
+            for(int j = 0; j < field[0].length; j++){
+                if(field[i][j].length() == 1){
+                    System.out.print(" " + field[i][j]);
+                    System.out.print(ANSI_CYAN_BACKGROUND + " " + ANSI_RESET);
+                }else if(field[i][j].charAt(0) > '9'){
+                    System.out.print(ANSI_YELLOW_BACKGROUND + field[i][j] + ANSI_RESET + ANSI_CYAN_BACKGROUND + " " + ANSI_RESET);
+                }else{
+                    System.out.print(field[i][j] + ANSI_CYAN_BACKGROUND + " " + ANSI_RESET);
+                }
+            }
+            System.out.print(ANSI_GREEN_BACKGROUND + "#" + ANSI_RESET);
+            System.out.println();
+            //
+            System.out.print("\t" +ANSI_GREEN_BACKGROUND + "#" + ANSI_RESET);
+            System.out.print(ANSI_CYAN_BACKGROUND + "                         " + ANSI_RESET);
+            System.out.print(ANSI_GREEN_BACKGROUND + "#" + ANSI_RESET);
+            System.out.println();
+        }
+        System.out.println("\t" + ANSI_GREEN_BACKGROUND + border + ANSI_RESET);
     }
+
+
 
 
 }

bd78e5a50fd4a9443f93435fc905a26ffaf47cc0
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2020-12-15 14:11:49 +0100

    added new methods in the FieldSeen

--- src/sk/kosisckaakademia/kkns/pexeso/FieldSeen.java
+++ src/sk/kosisckaakademia/kkns/pexeso/FieldSeen.java
@@ -31,32 +31,20 @@ public class FieldSeen extends Field {
 
 
     //a method for painting a gaming field player1 vs player2
+    //a method was replaced by a generic method
     public void paintField1(Player1 player1, Player2 player2){
 
     }
 
 
     //a method for painting a gaming field player1 vs computer
+    //a method was replaced by a generic method
     public void paintField2(Player1 player1, PcPlayer pcPlayer){
 
     }
 
-    //test
+    //test a generic method
     public <T extends Player> void paintField(Player1 player1, T player2){
-/*
-        String[][] f = getField();
-
-        System.out.println();
-        System.out.println("+-------------------------+" +"  "
-                +"\u001B[34m" +player1.getName() +": " +player1.getScore() +" "
-                +"\u001B[31m" +player2.getName() +": " +player2.getScore() +"\u001B[0m");
-        System.out.print("| ");
-        for(int j=0; j<f[0].length; j++) System.out.print(f[0][j]);
-
-
-
- */
-
         System.out.println();
         String border = "+-------------------------+";
         System.out.println("\t" + ANSI_GREEN_BACKGROUND + border + ANSI_RESET + " " +
@@ -87,6 +75,24 @@ public class FieldSeen extends Field {
     }
 
 
+
+    //a method which is used in case the cards are not same and wrong cards
+    //are in the red zone
+    public void paintFieldSeen1InCaseBlackPetersOrCardsAreNotTheSame(Player1 player1, Player2 player2, int index1, int index2, FieldGenerated fieldGenerated){
+
+        
+
+
+        hideWrongCards(index1, index2);
+    }
+
+
+
+
+    //a method which is used for hiding (turning) wrong cards
+    private void hideWrongCards(int index1, int index2){
+
+    }
 
 
 }
